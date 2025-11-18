4cedc7583f0159226cf33dd5680b683abc23f3bd
Ramiro <ramirotules@hotmail.com>
2025-11-17 23:23:40 -0300

    Se elimina la busqueda de archivos y carpetas por dificultades en la implementacion

--- include/utils/funcBackend.h
+++ include/utils/funcBackend.h
@@ -1,19 +1,6 @@
 #pragma once
 #include <iostream>
 
-
-/// Navega a traves de las carpetas en la linea de comandos para seleccionar la deseada,
-/// comenzando desde la carpeta actual.
-/// @return Ruta absoulta
-std::string selectorCarpeta();
-
-
-/// Navega a traves de las carpetas en la linea de comandos para seleccionar un archivo,
-/// comenzando desde la carpeta actual.
-/// @return Ruta absoulta
-std::string selectorArchivo();
-
-
 /// Realiza un backup de todos los datos actuales presentes en la carpeta raiz del programa.
 /// Pide seleccionar la carpeta donde esten los datos en caso de no encontrarlos.
 void backup();
